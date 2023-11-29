9f5d7cbbc5f178d1bd62fd60067c08378c43f7e7
Abhishek Singh <135206235+Abhishek31singh@users.noreply.github.com>
2023-11-29 12:33:32 +0530

    Update table.php

--- Final mini Project/table.php
+++ Final mini Project/table.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    *{
+        background-color: red;
+        color: aliceblue;
+    }
+</style>
+<body>
 <?php
 $servername="localhost";
 $username="root";
@@ -43,3 +57,5 @@ else{
 $conn->close();
 
 ?>
+</body>
+</html>
